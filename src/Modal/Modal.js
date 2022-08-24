d962c24e7f8aaf0218b052517780551c9a5ae500
Demion-L <padovds@ukr.net>
2022-08-24 16:09:00 +0300

    modal added

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './Modal.css';
 
 export default class Modal extends React.Component {
+    state = {
+        isOpen: false
+    }
 
+    render() {
+        return (
+            <button>qw</button>
+                )
+    }
 
 }
